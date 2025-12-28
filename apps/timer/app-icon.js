fd4e0a380738900885bc3a61d5fd0665b1f2b8ab
KittensAndCode <Soren.Bogwald@outlook.com>
2025-12-28 08:57:43 -0800

    Update app icon data in app-icon.js

--- apps/timer/app-icon.js
+++ apps/timer/app-icon.js
@@ -1,1 +1,1 @@
-require("heatshrink").decompress(atob("mEwwJC/AH4A/AH4AgA=="))
+require("heatshrink").decompress(atob("mEw4MA///xF9FstggwFDuEOAoc//gFJv/+AoZHBAgUB8/nwAFCBYIFCgYFB4AFHABdjCIPGAoPzAoPPAvpHFMpYFFPosAnk8NgYFdjEYfMo="))
